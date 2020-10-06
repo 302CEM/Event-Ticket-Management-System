9543a5b45bf8872ba144df72508b5bd40b88076e
leongdiyong <40769221+leongdiyong@users.noreply.github.com>
2020-10-06 20:10:44 +0800

    Add files via upload

---
+++ Public/contactus.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../Assets/header.php');
+
+?>
+<div id="aboutus"><h4>Contact me at <a href="mailto:amro.soliman@protonmail.com"><span class="name">NIcholas Leong</span></h4></div>
+
+
+
+<?php require_once('../Assets/footer.php'); ?>
